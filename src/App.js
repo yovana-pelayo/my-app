a71a9a6205ec42541a303ea1fd2c40a277c657dc
Yovana Pelayo <yovana_pelayo@yahoo.com>
2023-11-30 23:29:50 -0800

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      
+      MY PORTFOLIO
+      </header>
+      <div>
+        My name is yovana!
+      </div>
+    </div>
+  );
+}
+
+export default App;
