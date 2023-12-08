9fdb8d90c470768ed085a6c97257a0e30e312690
Yovana Pelayo <yovana_pelayo@yahoo.com>
2023-12-08 07:07:59 -0800

    removed a function that was not being used

--- src/Menu/Projects.jsx
+++ src/Menu/Projects.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { useNavigate } from 'react-router-dom';
+// import { useNavigate } from 'react-router-dom';
 import './Projects.css'
 
 const Projects = () => {
- const navigate = useNavigate();
+//  const navigate = useNavigate();
  return (
     <div className="container">
        <div className="title">
